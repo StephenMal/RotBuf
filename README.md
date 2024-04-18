44aaa7716a420174b8b6cfc9d28eb087c42eee88
StephenMal <Maldonado527@gmail.com>
2024-04-18 18:57:17 -0400

    Added more documentation

--- README.md
+++ README.md
@@ -12,16 +12,38 @@ fn create_rotating_buffer() -> RotatingBuffer {
 }
 ```
 
+## Enqueueing and Dequeueing
+
 The simplest way to use the `RotatingBuffer` is to treat it like a queue, enqueing and dequeing one byte at a time.
 
-```rust
+`enqueue` is very easy, just provide it with any `u8` (best representation for a singular byte).
 
+`dequeue` returns an `Option`, containing either the front most byte in Some, or, if empty, None.
+
+```rust
 rb = RotatingBuffer::new(10);
 rb.enqueue(50)?
-rb.dequeue()
-
+match rb.dequeue() {
+    Some(value) => println!("Look, we dequeued something: {}", value),
+    None => println!("Womp womp, we were empty."),
+}
 ```
 
-`dequeue` returns an Option, containing either the front most byte in Some, or, if empty, None.
+`enqueue` in most cases will return an empty [Ok] to signify it was successful.  If it reaches the capacity of the RotatingBuffer, it will return an Err with a RotatingBufferAtCapacity.  
 
-`enqueue` on the other hand returns an error enum.  At the moment, the only error that can
+```rust
+match rb.enqueue(50) {
+    Ok(()) => println!("The value was enqueued"),
+    Err(err) => println!("Oh no we must be at capacity: {}", err)
+}
+```
+
+The RotatingBufferAtCapacity is an Error, but you can reclaim the value you provided by using the `reclaim` fn
+
+```rust
+match rb.enqueue(50) {
+    Ok(()) => println!("The value was enqueued"),
+    Err(err) => println!("Oh no we couldn't enqueue this byte: {}", err.reclaim())
+}
+```
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,26 @@
-//! Hello
+#![doc = include_str!("../README.md")]
 
 use bytes::{BufMut, BytesMut};
 
+/// The [RotatingBuffer] is a queue implementation wrapping a [BytesMut].  
+/// 
+/// [RotatingBuffer::enqueue] and [RotatingBuffer::dequeue] will not require memory to
+/// be shifted.
 #[derive(Debug)]
 pub struct RotatingBuffer {
+    /// The buffer used to store the bytes.
     buffer: BytesMut,
+    /// The index of the head of the queue.  This represents the first value
+    /// that is officially part of the Queue (and in most cases, not the first value
+    /// of the buffer)
     head: usize,
+    /// The index of the tail.  This represents the first non-enqueued value
+    /// in the buffer that can be overwritten when we enqueue.  
     tail: usize,
+    /// The size of the queue.
     size: usize,
-    at_capacity: bool
+    /// Whether or not we are at capacity.
+    at_capacity: bool,
 }
 
 impl RotatingBuffer {
@@ -19,7 +31,7 @@ impl RotatingBuffer {
             head: 0,
             tail: 0,
             size: 0,
-            at_capacity: false
+            at_capacity: false,
         }
     }
 
@@ -52,10 +64,12 @@ impl RotatingBuffer {
         }
     }
 
+    /// Returns the head of the queue
     fn head(&self) -> usize {
         self.head
     }
 
+    /// Sets the head position in the queue
     fn set_head(&mut self, head: usize) {
         #[cfg(debug_assertions)]
         if head >= self.size {
@@ -64,6 +78,7 @@ impl RotatingBuffer {
         self.head = head;
     }
 
+    /// Sets the tail position in the queue
     fn set_tail(&mut self, tail: usize) {
         #[cfg(debug_assertions)]
         if tail >= self.size {
@@ -73,6 +88,8 @@ impl RotatingBuffer {
         self.tail = tail;
     }
 
+    /// Returns the index of the first position of the Queue, or None
+    /// if empty.
     fn first_indx(&self) -> Option<usize> {
         if self.is_empty() {
             None
@@ -99,6 +116,7 @@ impl RotatingBuffer {
         self.set_head((self.head + 1) % self.size);
     }
 
+    /// Returns the index one slot before the head
     pub(crate) fn prev_head(&self) -> usize {
         match self.head() {
             0 => self.size - 1,
@@ -106,7 +124,13 @@ impl RotatingBuffer {
         }
     }
 
-    /// Increments the tail
+    /// Increments the tail.
+    /// 
+    /// ## PANIC (DEBUG)
+    /// 
+    /// Although this should never be called when we are at capacity, if we are at capacity
+    /// and the head and the tail are at the same position, we panic, as we will then
+    /// be overwriting data.
     pub(crate) fn incr_tail(&mut self) {
         #[cfg(debug_assertions)]
         if self.head() == self.tail() && self.at_capacity() {
@@ -115,17 +139,19 @@ impl RotatingBuffer {
         self.set_tail((self.tail + 1) % self.size)
     }
 
-    /// Returns whether or not it is empty.
+    /// Returns whether or not the [RotatingBuffer] is empty
     pub fn is_empty(&self) -> bool {
         self.tail() == self.head() && !self.at_capacity()
     }
 
-    /// Returns the total capacity.
+    /// Returns the total capacity.  This is the number of elements we can enqueue (without dequeueing)
+    /// before we can no longer enqueue anymore elements.  Once we reach this capacity, you must dequeue
+    /// in order to fit into the [RotatingBuffer] without resizing.
     pub fn capacity(&self) -> usize {
         self.size
     }
 
-    /// Returns the number of elements in the Queue
+    /// Returns the number of elements currently in the Queue.
     pub fn len(&self) -> usize {
         match (self.tail(), self.head()) {
             (tail, head) if tail > head => tail - head,
@@ -137,13 +163,17 @@ impl RotatingBuffer {
                 } else {
                     0
                 }
-            },
+            }
             (tail, head) => {
                 unreachable!("`tail` ({}) must by >, <, or == to `head` ({})", tail, head)
             }
         }
     }
 
+    /// Peek the value stored at a given position.
+    /// 
+    /// Note: `pos` is the position in the queue, not necessarily the index in the buffer,
+    /// and starts at 0 where 0 represents the head of the queue.
     pub fn peek_pos(&self, pos: usize) -> Option<u8> {
         match (pos, self.len()) {
             (0, _) => self.peek(),
@@ -154,15 +184,25 @@ impl RotatingBuffer {
         }
     }
 
+    /// Peeks the first value in the queue.  Returns [None] if the queue is empty.
+    /// 
+    /// This method should be preferred over calling [RotatingBuffer::peek_pos] at position 0.
     pub fn peek(&self) -> Option<u8> {
         self.get_from_index(self.first_indx()?)
     }
 
+    /// Peeks the last value in the queue.  Returns [None] if the queue is empty.
+    /// 
+    /// This should be preferred over calling [RotatingBuffer::peek_pos] at position (last position)
     pub fn peek_last(&self) -> Option<u8> {
         self.get_from_index(self.last_indx()?)
     }
 
-    /// Returns either Some(value) or None if empty
+    /// Returns the front-most value from the Queue in a Some.  If the [RotatingBuffer] is empty, 
+    /// we will return a [None].
+    /// 
+    /// This should be fairly cheap to run, as no memory in the buffer is altered.  Once an item
+    /// is dequeued, every sequential item's position is one less than it was before.
     pub fn dequeue(&mut self) -> Option<u8> {
         match self.get_from_index(self.first_indx()?) {
             Some(value) => {
@@ -178,6 +218,15 @@ impl RotatingBuffer {
         }
     }
 
+    /// Sets the value at an index, not the queue position.  Can only set values from 0 
+    /// to the current buffer length + 1.
+    /// 
+    /// ## PANICS
+    /// 
+    /// In the event that you try to set a value outside of the current buffer length, a
+    /// panic will occur.  The [RotatingBuffer] is a queue implementation, meaning there
+    /// should never be a time we are writing further in the buffer than the most recent
+    /// writing.
     fn set_value(&mut self, index: usize, value: u8) {
         match (index, self.buffer.len()) {
             (index, len) if index == len => {
@@ -192,15 +241,25 @@ impl RotatingBuffer {
         }
     }
 
+    /// Returns a [bool] representing whether the [RotatingBuffer] is at capacity.  This
+    /// means that enqueueing another value will cause an [Err].
     pub fn at_capacity(&self) -> bool {
         match self.at_capacity {
             #[cfg(debug_assertions)]
-            true if self.tail() != self.head() => unreachable!("at capacity is true and shouldn't be"),
-            boolean => boolean
+            true if self.tail() != self.head() => {
+                unreachable!("at capacity is true and shouldn't be")
+            }
+            boolean => boolean,
         }
     }
 
-    /// Enqueues an item into the [RotatingBuffer].  Returns an Error if at capacity.
+    /// Enqueues an item into the [RotatingBuffer].  Returns an [Err] with a 
+    /// [RotatingBufferAtCapacity] if at capacity.
+    /// 
+    /// Enqueueing should be fairly cheap, as we initialize the internal buffer 
+    /// with the maximum size given in the constructor, so we will always be
+    /// either replacing a pre-existing and already dequeued value, or we will
+    /// be placing a value into already allocated memory.
     pub fn enqueue(&mut self, value: u8) -> Result<(), RotatingBufferAtCapacity> {
         // If we are at capacity, return error, otherwise add tail
         if self.at_capacity() {
@@ -218,13 +277,10 @@ impl RotatingBuffer {
             // Return okay
             Ok(())
         }
-
-        
     }
 }
 
-
-/// [RotatingBufferAtCapacity] is a struct that represents an error.  It is returned whenever 
+/// [RotatingBufferAtCapacity] is a struct that represents an error.  It is returned whenever
 /// there was an attempt to enqueue a [RotatingBuffer] despite it being at capacity.  In
 /// this instance, the value given is returned to the user, and can be reclaimed using
 /// [RotatingBufferAtCapacity::reclaim].
@@ -240,7 +296,11 @@ impl RotatingBufferAtCapacity {
 
 impl std::fmt::Display for RotatingBufferAtCapacity {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "RotatingBuffer at capacity, returned input: `{}`", self.0)
+        write!(
+            f,
+            "RotatingBuffer at capacity, returned input: `{}`",
+            self.0
+        )
     }
 }
 
@@ -362,7 +422,6 @@ mod test {
         rb.enqueue(5).unwrap();
         assert_eq!(rb.len(), 2);
         rb.enqueue(6).unwrap();
-
     }
 
     #[test]
